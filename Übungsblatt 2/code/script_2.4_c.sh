cf8143cc3bb78c21a0d0aedece1ddc932500c301
CodingTil <36734749+CodingTil@users.noreply.github.com>
2020-05-09 18:00:32 +0200

    Adding 2.4 c-d

--- Übungsblatt 2/code/script_2.4_c.sh
+++ Übungsblatt 2/code/script_2.4_c.sh
@@ -2,15 +2,16 @@
 
 # Parameter is team size
 teams () {
-    FILE=$(sort teamnamen.txt | uniq -c)
-    FILE="$(sed 's/^ *//' <<<$FILE)"
-    #printf "${FILE}\n"
-    AMOUNT=$(grep -c "^$1" "^$FILE")
-    echo $AMOUNT
+    FILE=$(sort teamnamen.txt | uniq -c | sed 's/^ *//' | grep -c "^$1")
+    echo ${FILE}
 }
 
 if [ $# -eq 0 ]; then
     teams 3
 else
-    teams $1
+    if [ $1 -eq "0" ]; then
+        echo $(grep -cvP '\S' teamnamen.txt)
+    else 
+        teams "$1"
+    fi
 fi
---
+++ Übungsblatt 2/code/script_2.4_d.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+for i in $(seq 0 3); do
+    source ./script_2.4_c.sh $i
+done
